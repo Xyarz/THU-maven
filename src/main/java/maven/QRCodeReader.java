55f4ef9e8a937d80e1ecdd5f0943e2457d0e50e6
Lars Andersen <lars-andersen@gmx.de>
2021-03-25 13:45:09 +0100

    Webcam QR Code reader implemented
    added the sarxos Webcam lib to integrate the option to read QR codes via webcam

--- src/main/java/maven/QRCodeReader.java
+++ src/main/java/maven/QRCodeReader.java
@@ -1,5 +1,6 @@
 package maven;
 
+import com.github.sarxos.webcam.Webcam;
 import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
@@ -25,16 +26,40 @@ public class QRCodeReader {
     }
 
     public static void main(String[] args) {
-        try {
-            File file = new File("src/test/resources/qr.png");
-            String decodedText = decodeQRCode(file);
-            if(decodedText == null) {
-                System.out.println("No QR Code found in the image");
-            } else {
-                System.out.println("Decoded text = " + decodedText);
-            }
-        } catch (IOException e) {
-            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
-        }
+//        try {
+//            File file = new File("src/test/resources/qr.png");
+//            String decodedText = decodeQRCode(file);
+//            if(decodedText == null) {
+//                System.out.println("No QR Code found in the image");
+//            } else {
+//                System.out.println("Decoded text = " + decodedText);
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+//        }
+//    }
+    	Webcam webcam = Webcam.getDefault(); // non-default (e.g. USB) webcam can be used too
+    	webcam.open();
+
+    	Result result = null;
+    	BufferedImage image = null;
+
+    	if (webcam.isOpen()) {
+    	    if ((image = webcam.getImage()) == null) {
+//    	        continue;
+    	    }
+
+    	    LuminanceSource source = new BufferedImageLuminanceSource(image);
+    	    BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+    	    try {
+    	        result = new MultiFormatReader().decode(bitmap);
+    	    } catch (NotFoundException e) {
+    	        // fall thru, it means there is no QR code in image
+    	    }
+    	}
+
+    	if (result != null) {
+    		 System.out.println("QR code data is: " + result.getText());
+    	}
     }
 }
